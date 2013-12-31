c8b94bc8bcc0fb06cccd1a9e4331ba53e0e4f2ca
Christopher Groskopf <staringmonkey@gmail.com>
2013-12-31 10:14:24 -0600

    Simple search UI.

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,13 @@
-var LAT = 32.35;
-var LNG = -95.3;
+var $search_form;
+var $lat;
+var $lng;
+var $distance;
+var $hours_back;
+var $photos;
 
-var DISTANCE = 50000; // 50km
 var DAYS_TO_GO_BACK = 5;
 
-function updateInstagrams(since, lat, lng) {
+function updateInstagrams(lat, lng, distance, since) {
     var now = Math.round((new Date()).getTime() / 1000);
 
     $.ajax({
@@ -12,7 +15,7 @@ function updateInstagrams(since, lat, lng) {
         data: {
             lat: lat,
             lng: lng,
-            distance: DISTANCE,
+            distance: distance,
             client_id: INSTAGRAM_CLIENT_ID,
             min_timestamp: since,
             max_timestamp: now 
@@ -20,7 +23,7 @@ function updateInstagrams(since, lat, lng) {
         dataType: 'jsonp',
         success: function(data) {
             // TODO: process pagination?
-            console.log(data);
+            console.log(data['meta']);
             renderInstagrams(data['data']);
         }
     });
@@ -33,14 +36,36 @@ function renderInstagrams(instagrams) {
         html += JST.instagram(instagrams[i]);
     }
         
-    $('#content').html(html);
+    $photos.html(html);
+}
+
+function on_search_form_submit(e) {
+    var lat = parseFloat($lat.val());
+    var lng = parseFloat($lng.val());
+    var distance = parseFloat($distance.val()) * 1000;
+    var hours_back = parseFloat($hours_back.val());
+
+    if (hours_back > 168) {
+        alert('Can\'t search photos from more than 7 days (168 hours) back.');
+        return false;
+    }
+
+    var time_to_go_back = 60 * 60 * hours_back;
+    var since = Math.round((new Date()).getTime() / 1000) - time_to_go_back;
+
+    $photos.empty();
+    updateInstagrams(lat, lng, distance, since);
+
+    return false;
 }
 
 $(function() {
-    console.log(INSTAGRAM_CLIENT_ID);
+    $search_form = $('#search');
+    $lat = $('#lat');
+    $lng = $('#lng');
+    $distance = $('#distance');
+    $hours_back = $('#hours-back');
+    $photos = $('#photos');
 
-    var time_to_go_back = 60 * 60 * 24 * DAYS_TO_GO_BACK;
-    var since = Math.round((new Date()).getTime() / 1000) - time_to_go_back;
-
-    updateInstagrams(since, LAT, LNG);
+    $search_form.on('submit', on_search_form_submit);  
 });
